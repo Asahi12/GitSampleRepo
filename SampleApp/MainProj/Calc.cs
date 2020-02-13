5f985e5fc5cf472b44c619faf074170b0d22610b
asahi mai <mai.asahi@jbs.com>
2020-02-13 11:43:17 +0900

    add calc.cs

---
+++ SampleApp/MainProj/Calc.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainProj
+{
+    public class Calc
+    {
+        public int CalcNum(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
